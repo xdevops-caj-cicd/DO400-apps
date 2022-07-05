4d022363fc653f586d3bece94861736b72869790
Administrator <admin@example.com>
2022-07-05 10:03:49 +0800

    Add greeting-service

---
+++ quality-checks/src/main/java/com/redhat/simple/calculator/BasicCalculator.java
@@ -0,0 +1,11 @@
+package com.redhat.simple.calculator;
+
+import java.util.Random;
+
+public final class BasicCalculator extends Calculator {
+    public int random() {
+        Random r = new Random();
+
+        return r.nextInt();
+    }
+}
